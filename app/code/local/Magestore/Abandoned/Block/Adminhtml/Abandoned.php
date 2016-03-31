3e95ceb44ad77eb425764161a2c3c712f3c3e851
Vincenkt <richardmagento@gmail.com>
2016-03-31 08:25:03 +0700

    av

--- app/code/local/Magestore/Abandoned/Block/Adminhtml/Abandoned.php
+++ app/code/local/Magestore/Abandoned/Block/Adminhtml/Abandoned.php
@@ -5,7 +5,7 @@ class Magestore_Abandoned_Block_Adminhtml_Abandoned extends Mage_Adminhtml_Block
   {
     $this->_controller = 'adminhtml_abandoned';
     $this->_blockGroup = 'abandoned';
-    $this->_headerText = Mage::helper('membership')->__('Abandoned Manager');
+    $this->_headerText = Mage::helper('abandoned')->__('Abandoned Manager');
     parent::__construct();
   }
 }
